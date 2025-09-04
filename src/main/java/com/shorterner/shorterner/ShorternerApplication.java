4878629730e8b5a9ba753aaf14e5afd930c2d169
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2025-09-04 11:56:04 -0400

    feat(init): initialize project with dependencies

---
+++ src/main/java/com/shorterner/shorterner/ShorternerApplication.java
@@ -0,0 +1,13 @@
+package com.shorterner.shorterner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorternerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorternerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shorterner/shorterner/ShorternerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shorterner.shorterner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorternerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
